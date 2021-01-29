b3556e7dbfd3e33ec7d5aaf0a0c10e6230217d43
Andy Denham <andywd7@gmail.com>
2021-01-29 15:44:24 +0000

    main cover image for mobile

--- resources/views/partials/cover.blade.php
+++ resources/views/partials/cover.blade.php
@@ -1,1 +1,45 @@
-<div style="background-image:url(@asset('images/mcg-bg.jpg')); background-size:cover; height:500px; width:500px;"></div>
+<div class="cover" style="background-image:url(@asset('images/mcg-bg.jpg');">
+  <div class="content">
+    <p class="content__eyebrow">01 Welcome</p>
+    <h1 class="content__heading">Creating Human </br>engagement.</h1>
+    <p class="content__tagline">through technology.</p>
+    <div class="content__link text-sm">
+      <a href="#">Discover MCG</a>
+    </div>
+  </div>
+  <div class="contain">
+    <div class="right-layer"></div>
+    <div class="right-layer right-layer--2">
+      <button type="button" class="marker active">
+        <span class="text-sm">About MCG</span>
+        <span class="text-md">01</span>
+      </button>
+    </div>
+    <div class="right-layer right-layer--3">
+      <button type="button" class="marker">
+        <span class="text-sm">Our Brands</span>
+        <span class="text-md">02</span>
+      </button>
+    </div>
+    <div class="right-layer right-layer--4">
+      <button type="button" class="marker">
+        <span class="text-sm">Explore Careers</span>
+        <span class="text-md">03</span>
+      </button>
+      <div class="text">
+        <div class="down">Human</div>
+        <div class="up">Engagement</div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="home-cover" style="background-image:url(http://localhost/mcg/wp-content/uploads/2021/01/mcg-min-scaled.jpg);">
+  <div class="testt">
+    <p class="eyebrow">02 Welcome</p>
+    <h1 class="heading">Creating Human </br>engagement.</h1>
+    <p class="tagline">through technology.</p>
+    <div class="link text-sm">
+      <a href="#">Discover MCG</a>
+    </div>
+  </div>
+</div> -->
