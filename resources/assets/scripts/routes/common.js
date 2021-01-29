d71d9460924500057505efd2d6c1575ad827491c
Andy Denham <andywd7@gmail.com>
2021-01-29 16:59:16 +0000

    enable nav backdrop

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,16 +3,25 @@ export default {
     // JavaScript to be fired on all pages
     const navToggle = document.querySelector('.header__toggle-nav');
     const navbar = document.querySelector('.navbar');
+    const navBackdrop = document.querySelector('.navbar__backdrop');
 
-    navToggle.addEventListener('click', () => {
+    navToggle.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
       navToggle.setAttribute(
         'aria-expanded',
         navToggle.getAttribute('aria-expanded') === 'false'
         ? 'true'
         : 'false'
       )
+      navBackdrop.classList.toggle('show');
       navbar.classList.toggle('open');
     });
+
+    navBackdrop.onclick = () => {
+      navBackdrop.classList.remove('show');
+      navbar.classList.remove('open');
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
